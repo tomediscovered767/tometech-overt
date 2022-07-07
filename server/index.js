c44daf4d04c6f8485e4d3c4ed716bf60473cc468
Tome Discovered <tomediscovered@gmail.com>
2022-07-07 12:45:16 -0500

    server route *

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join('build', 'index.html'));
     });
 }
 else{
