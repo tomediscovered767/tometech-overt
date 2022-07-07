2f037635e5e7ce9d136254c458756eca1112fc63
Tome Discovered <tomediscovered@gmail.com>
2022-07-07 11:05:59 -0500

    server route *

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ app.use(bodyParser.json());
 require("./routes/AuthApiRoutes.js")(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => { res.sendFile(path.resolve('build', 'index.html')); });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+  });
 }
 else{
   require('dotenv').config();
