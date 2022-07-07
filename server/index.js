816eb3566fc76c850610dbdb116807e630ac7965
Tome Discovered <tomediscovered@gmail.com>
2022-07-07 12:42:48 -0500

    React-Router

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 require("./routes/AuthApiRoutes.js")(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 }
 else{
   require('dotenv').config();
